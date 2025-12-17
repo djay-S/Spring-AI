a34d4ad5636d58ac565bfaeb5585342975572bdd
djay <dhananjaysinghkushwaha2@gmail.com>
2025-12-17 21:10:17 +0530

    gemini-genai: initial setup, added `ChatController`

---
+++ Gemini-Gen-AI/src/main/java/org/demo/geminigenai/chat/ChatController.java
@@ -0,0 +1,22 @@
+package org.demo.geminigenai.chat;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping("/chat")
+    public String chat() {
+        return chatClient.prompt()
+                .user("What is your name?")
+                .call()
+                .content();
+    }
+}
