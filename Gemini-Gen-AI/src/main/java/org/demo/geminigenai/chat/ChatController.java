aad9eeb2f840ee888ae265eea4da701324dbc430
djay <dhananjaysinghkushwaha2@gmail.com>
2025-12-17 21:21:06 +0530

    gemini-genai: streaming response

--- Gemini-Gen-AI/src/main/java/org/demo/geminigenai/chat/ChatController.java
+++ Gemini-Gen-AI/src/main/java/org/demo/geminigenai/chat/ChatController.java
@@ -3,6 +3,9 @@ package org.demo.geminigenai.chat;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
 
 @RestController
 public class ChatController {
@@ -18,5 +21,15 @@ public class ChatController {
                 .user("What is your name?")
                 .call()
                 .content();
+    }
+
+//    To get the streaming response use: curl -N --location 'localhost:8080/stream'
+    @GetMapping("/stream")
+    Flux<String> stream() {
+        return chatClient.prompt()
+                .user("I am visiting Hyderabad. Can you suggest me 10 restaurants to visit.")
+                .stream()
+                .content()
+                .delayElements(Duration.ofSeconds(1));
     }
 }
