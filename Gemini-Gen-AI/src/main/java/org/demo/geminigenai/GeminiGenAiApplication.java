79c6c659c8ef64a002aa7a63c23fdb1fe36ec432
djay <dhananjaysinghkushwaha2@gmail.com>
2025-12-17 20:35:55 +0530

    gemini-genai: added project files

---
+++ Gemini-Gen-AI/src/main/java/org/demo/geminigenai/GeminiGenAiApplication.java
@@ -0,0 +1,13 @@
+package org.demo.geminigenai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeminiGenAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeminiGenAiApplication.class, args);
+    }
+
+}
---
+++ Gemini-Gen-AI/src/test/java/org/demo/geminigenai/GeminiGenAiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.geminigenai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeminiGenAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
