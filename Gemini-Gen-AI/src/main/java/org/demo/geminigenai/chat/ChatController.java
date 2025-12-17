02691b200b3e94588da7418ea00ab2c83eb301f5
djay <dhananjaysinghkushwaha2@gmail.com>
2025-12-17 21:25:12 +0530

    gemini-genai: `ChatResponse` response

--- Gemini-Gen-AI/src/main/java/org/demo/geminigenai/chat/ChatController.java
+++ Gemini-Gen-AI/src/main/java/org/demo/geminigenai/chat/ChatController.java
@@ -1,6 +1,7 @@
 package org.demo.geminigenai.chat;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -31,5 +32,13 @@ public class ChatController {
                 .stream()
                 .content()
                 .delayElements(Duration.ofSeconds(1));
+    }
+
+    @GetMapping("/response")
+    public ChatResponse getChatResponse() {
+        return chatClient.prompt()
+                .user("What is the current timestamp?")
+                .call()
+                .chatResponse();
     }
 }
