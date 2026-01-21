a1cd1e0e117a7a1f4ea05c3452ee56e85bea61b9
djay <dhananjaysinghkushwaha2@gmail.com>
2026-01-21 21:18:16 +0530

    gemini-genai: Restaurant AI - for RAG on vector data, added `QuestionAnswerAdvisor`.

--- Gemini-Gen-AI/src/main/java/org/demo/geminigenai/restaurant/RestaurantController.java
+++ Gemini-Gen-AI/src/main/java/org/demo/geminigenai/restaurant/RestaurantController.java
@@ -2,7 +2,9 @@ package org.demo.geminigenai.restaurant;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,8 +19,12 @@ public class RestaurantController {
     private final String SYSTEM_PROMPT =
             "Your name is Raj. You are a waiter at 'Desi Restaurant'. Your task is to take orders from customers and suggest them dishes based on customer preference and restaurant menu.";
 
-    public RestaurantController(ChatClient.Builder builder, PromptChatMemoryAdvisor chatMemory) {
-        this.chatClient = builder.defaultAdvisors(chatMemory).build();
+    public RestaurantController(
+            ChatClient.Builder builder, PromptChatMemoryAdvisor chatMemory, VectorStore vectorStore) {
+        this.chatClient = builder.defaultAdvisors(
+                        QuestionAnswerAdvisor.builder(vectorStore).build())
+                .defaultAdvisors(chatMemory)
+                .build();
     }
 
     @GetMapping("/assistant")
